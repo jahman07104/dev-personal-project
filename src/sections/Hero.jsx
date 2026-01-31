5b4ce333b8f699b7a5af8394a268d65225af3365
bwoysie <bwoysie876@gmail.com>
2026-01-30 21:54:06 -0600

    git test

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -76,7 +76,8 @@ export const Hero = () => {
             {/* CTAs */}
             <div className="flex- flex-wrap gap-4 animate-fade-in animation-delay-300 ">
               <Button size="lg">
-                Contact Me <ArrowRight className="w-5 h-5" />
+                Contact Me now
+                <ArrowRight className="w-5 h-5" />
               </Button>
               <AnimatedBorderButton />
             </div>
